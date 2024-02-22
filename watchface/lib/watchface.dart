fcd097190f8b4dcce168097f02e696b2e2582af8
raymond <raymondtruongdev@gmail.com>
2024-02-22 10:43:17 +0700

    init

--- watchface/lib/watchface.dart
+++ watchface/lib/watchface.dart
@@ -1,7 +1,20 @@
 library watchface;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class WatchFace extends StatefulWidget {
+  const WatchFace({super.key});
+
+  @override
+  State<WatchFace> createState() => _WatchFaceState();
+}
+
+class _WatchFaceState extends State<WatchFace> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      child: const Text('WatchFace'),
+    );
+  }
 }
